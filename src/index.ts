5b2f6e70d8a5b0d3e58fa7ac5a409db57b1aa2fd
Danakt Saushkin <danakt@protonmail.com>
2020-02-20 19:10:45 +0300

    Fix umd and commonjs exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createVKConnect } from './connect';
 import { createCustomEventPolyfill } from './custom-event';
 import { version } from '../package.json';
+import { applyMiddleware } from './applyMiddleware';
 import './custom-event';
 
 // Applying CustomEvent polyfill
@@ -8,13 +9,22 @@ if (typeof window !== 'undefined' && !window.CustomEvent) {
   (window as any).CustomEvent = createCustomEventPolyfill();
 }
 
+// VK Connect API
+const connect = createVKConnect(version);
+
 // Export typings
 export * from './types/data';
 export * from './types/connect';
 export * from './types/middleware';
 
-// Export middleware types and `applyMiddleware` function
-export * from './applyMiddleware';
+// Named ESM export middleware types and `applyMiddleware` function
+export { applyMiddleware } from './applyMiddleware';
+// Default ESM export VK Connect API
+export default connect;
 
-// Export VK Connect API
-export default createVKConnect(version);
+// Mixed export
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { ...connect };
+  module.exports.default = { ...connect };
+  module.exports.applyMiddleware = applyMiddleware;
+}
