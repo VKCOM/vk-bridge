13345d79998a6e68c2567998110cceed3e438649
Danakt Saushkin <danakt@protonmail.com>
2020-02-20 21:01:19 +0300

    Add jest and tests for valid exports

---
+++ src/__tests__/dist.ts
@@ -0,0 +1,33 @@
+import connectEsm, { applyMiddleware as applyMiddlewareEsm } from '../../';
+const { applyMiddleware: applyMiddlewareUmd, ...connectUmd } = require('../../dist/index.umd');
+const { applyMiddleware: applyMiddlewareCmj, ...connectCmj } = require('../../dist/index');
+
+test('Valid esm export', () => {
+  expect(typeof connectEsm.send).toBe('function');
+  expect(typeof connectEsm.subscribe).toBe('function');
+  expect(typeof connectEsm.unsubscribe).toBe('function');
+  expect(typeof connectEsm.supports).toBe('function');
+  expect(typeof connectEsm.isWebView).toBe('function');
+
+  expect(typeof applyMiddlewareEsm).toBe('function');
+});
+
+test('Valid umd export', () => {
+  expect(typeof connectUmd.send).toBe('function');
+  expect(typeof connectUmd.subscribe).toBe('function');
+  expect(typeof connectUmd.unsubscribe).toBe('function');
+  expect(typeof connectUmd.supports).toBe('function');
+  expect(typeof connectUmd.isWebView).toBe('function');
+
+  expect(typeof applyMiddlewareUmd).toBe('function');
+});
+
+test('Valid commonjs export', () => {
+  expect(typeof connectCmj.send).toBe('function');
+  expect(typeof connectCmj.subscribe).toBe('function');
+  expect(typeof connectCmj.unsubscribe).toBe('function');
+  expect(typeof connectCmj.supports).toBe('function');
+  expect(typeof connectCmj.isWebView).toBe('function');
+
+  expect(typeof applyMiddlewareCmj).toBe('function');
+});
