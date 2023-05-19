a27642147c1dd0c847d4c63bb7e6186ccae216b8
miholeus <nigel.webmaster@gmail.com>
2023-05-19 12:42:50 +0300

    new banners support (#337)
    * new banners support
    
    * banner params update

--- src/types/data.ts
+++ src/types/data.ts
@@ -402,7 +402,23 @@ export type ShowNativeAdsRequest = {
 export type CheckNativeAdsRequest = {
   ad_format: EAdsFormats;
   use_waterfall?: boolean;
+};
+
+export enum BannerAdLayoutType {
+  RESIZE = 'resize',
+  OVERLAY = 'overlay',
 }
+
+export enum BannerAdLocation {
+  TOP = 'top',
+  BOTTOM = 'bottom',
+}
+
+export type ShowBannerAdRequest = {
+  banner_location: BannerAdLocation;
+  layout_type?: BannerAdLayoutType;
+  can_close?: boolean;
+};
 
 export type OrderRequestOptions = {
   /** Always `item` */
@@ -964,6 +980,17 @@ export type ConversionHitResponse = {
   result: true;
 };
 
+export type VKWebAppShowBannerAdResponse = {
+  result: boolean;
+  banner_width: number;
+  banner_height: number;
+  banner_location: BannerAdLocation;
+  layout_type: BannerAdLayoutType;
+};
+
+export type VKWebAppCheckBannerAdResponse = VKWebAppShowBannerAdResponse;
+export type VKWebAppHideBannerAdResponse = VKWebAppShowBannerAdResponse;
+
 export type VKWebAppShowOrderBoxResponse = {
   status: OrderBoxShowingStatus;
   order_id: string;
@@ -1040,6 +1067,9 @@ export type RequestPropsMap = {
   VKWebAppShowInviteBox: {};
   VKWebAppShowLeaderBoardBox: { user_result: number };
   VKWebAppShowMessageBox: MessageRequestOptions;
+  VKWebAppCheckBannerAd: {},
+  VKWebAppHideBannerAd: {},
+  VKWebAppShowBannerAd: ShowBannerAdRequest;
   VKWebAppShowNativeAds: ShowNativeAdsRequest;
   VKWebAppCheckNativeAds: CheckNativeAdsRequest;
   VKWebAppShowOrderBox: OrderRequestOptions;
@@ -1127,6 +1157,9 @@ export type ReceiveDataMap = {
   VKWebAppShowInviteBox: { success: true };
   VKWebAppShowLeaderBoardBox: { success: boolean };
   VKWebAppShowMessageBox: { result: true };
+  VKWebAppCheckBannerAd: VKWebAppCheckBannerAdResponse,
+  VKWebAppHideBannerAd: VKWebAppHideBannerAdResponse,
+  VKWebAppShowBannerAd: VKWebAppShowBannerAdResponse;
   VKWebAppShowNativeAds: { result: true };
   VKWebAppCheckNativeAds: { result: boolean };
   VKWebAppShowOrderBox: VKWebAppShowOrderBoxResponse;
@@ -1258,6 +1291,9 @@ export type ReceiveEventMap = EventReceiveNames<'VKWebAppInit', 'VKWebAppInitRes
     'VKWebAppShowLeaderBoardBoxFailed'
   > &
   EventReceiveNames<'VKWebAppShowMessageBox', 'VKWebAppShowMessageBoxResult', 'VKWebAppShowMessageBoxFailed'> &
+  EventReceiveNames<'VKWebAppCheckBannerAd', 'VKWebAppCheckBannerAdResult', 'VKWebAppCheckBannerAdFailed'> &
+  EventReceiveNames<'VKWebAppHideBannerAd', 'VKWebAppHideBannerAdResult', 'VKWebAppHideBannerAdFailed'> &
+  EventReceiveNames<'VKWebAppShowBannerAd', 'VKWebAppShowBannerAdResult', 'VKWebAppShowBannerAdFailed'> &
   EventReceiveNames<'VKWebAppShowNativeAds', 'VKWebAppShowNativeAdsResult', 'VKWebAppShowNativeAdsFailed'> &
   EventReceiveNames<'VKWebAppCheckNativeAds', 'VKWebAppCheckNativeAdsResult', 'VKWebAppCheckNativeAdsFailed'> &
   EventReceiveNames<'VKWebAppShowOrderBox', 'VKWebAppShowOrderBoxResult', 'VKWebAppShowOrderBoxFailed'> &
