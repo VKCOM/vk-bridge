61ec012c03399b6513f32da7a08e8642ff0895cc
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-07-24 21:42:45 +0300

    feat(vk-bridge): create parser for launch params
    Create `parseURLSearchParamsForGetLaunchParams()`.
    
    Function returns [launch params](https://dev.vk.com/mini-apps/development/launch-params).

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,4 +11,7 @@ export * from './types/middleware';
 export * from './types/deprecated';
 
 export { applyMiddleware } from './applyMiddleware';
+
+export { parseURLSearchParamsForGetLaunchParams } from './parseURLSearchParamsForGetLaunchParams';
+
 export { bridge as default };
