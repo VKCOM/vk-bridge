1bc1481813a950c8cfe235eae9750ed8e8fdd041
And R. <64421792+quick6response@users.noreply.github.com>
2025-07-17 09:39:05 +0300

    Update data.ts

--- packages/core/src/types/data.ts
+++ packages/core/src/types/data.ts
@@ -1271,7 +1271,7 @@ export type ReceiveDataMap = {
     | { available: 1; lat: number; long: number; accuracy: number };
   VKWebAppGetGrantedPermissions: GetGrantedPermissionsResponse;
   VKWebAppGetPersonalCard: PersonalCardData;
-  VKWebAppGetPhoneNumber: { phone_number: string; sign: string; isValid: boolean };
+  VKWebAppGetPhoneNumber: { phone_number: string; sign: string; isValid?: boolean };
   VKWebAppGetUserInfo: UserInfo;
   VKWebAppJoinGroup: { result: true };
   VKWebAppLeaveGroup: { result: true };
